05d52f2f403117021d52aa5008d22cdf3b32b1c2
Navjot Gill <Navjot Gill@Raman>
2019-05-22 10:55:17 -0400

    R3-Green

--- src/SoftwareTest.java
+++ src/SoftwareTest.java
@@ -13,6 +13,13 @@ public class SoftwareTest {
 			double finalTotal = subtotal - discount;
 			return finalTotal;
 		}
+		else if(quantity >= 19 && quantity <= 49)
+		{
+			double subtotal = 99 * quantity;
+			double discount = subtotal * 0.30;
+			double finalTotal = subtotal - discount;
+			return finalTotal;
+		}
 		return ( 99.0 * quantity);
 	}
 	
--- test/SoftwareSaleTest.java
+++ test/SoftwareSaleTest.java
@@ -5,9 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SoftwareSaleTest {
-
+	SoftwareTest s = new SoftwareTest();
 	@Before
 	public void setUp() throws Exception {
+	
 	}
 
 	@After
@@ -16,26 +17,26 @@ public class SoftwareSaleTest {
 
 	@Test
 	public void test() {
-//		SoftwareTest s1 = new SoftwareTest();
-//		double finalPrice = s1.calculatePrice(2);
-//		//when comapring decimal in juint you must add the third param to the assertEqual
-//		//for the third param use the minimum number like 0.000001 or 0
-//		assertEquals(198, finalPrice,0);
-		//
 		
-		//20% discount
-		SoftwareTest s2 = new SoftwareTest();
-		double finalPrice2 = s2.calculatePrice(12);
+		double finalPrice = s.calculatePrice(2);
+		//when comapring decimal in juint you must add the third param to the assertEqual
+		//for the third param use the minimum number like 0.000001 or 0
+		assertEquals(198, finalPrice,0);
+		
+		
+		//buy ,20% discount
+		
+		double finalPrice2 = s.calculatePrice(12);
 		//when comapring decimal in juint you must add the third param to the assertEqual
 		//for the third param use the minimum number like 0.000001 or 0
 		assertEquals(950.4, finalPrice2,0);
 		
-		//30% discount
-		SoftwareTest s3 = new SoftwareTest();
-		double finalPrice3 = s3.calculatePrice(30);
+		// buy 20-48 ,30% discount
+	
+		double finalPrice3 = s.calculatePrice(30);
 		//when comapring decimal in juint you must add the third param to the assertEqual
 		//for the third param use the minimum number like 0.000001 or 0
-		assertEquals(950.4, finalPrice3,0);
+		assertEquals(2079, finalPrice3,0);
 	}
 	
 	
