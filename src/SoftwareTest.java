8dedbb80b88ba5c4332f58587a78f38d4a81fac5
Navjot Gill <Navjot Gill@Raman>
2019-05-22 11:04:31 -0400

    R3-Refactor

--- src/SoftwareTest.java
+++ src/SoftwareTest.java
@@ -1,26 +1,30 @@
 
 public class SoftwareTest {
 
-
+	
 	
 	//return the total price of the software package
 	public double calculatePrice(int quantity)
 	{
+		double subtotal = 99 * quantity;
+		double discount = 0.0;
+		
+		//calculate discount
 		if(quantity >= 10 && quantity <= 19)
 		{
-			double subtotal = 99 * quantity;
-			double discount = subtotal * 0.20;
-			double finalTotal = subtotal - discount;
-			return finalTotal;
+			//20%
+			discount = subtotal * 0.20;
+		
 		}
 		else if(quantity >= 19 && quantity <= 49)
 		{
-			double subtotal = 99 * quantity;
-			double discount = subtotal * 0.30;
-			double finalTotal = subtotal - discount;
-			return finalTotal;
+			//30%
+			 discount = subtotal * 0.30;
+			
 		}
-		return ( 99.0 * quantity);
+		double finalTotal = subtotal - discount;
+		return finalTotal;
+		
 	}
 	
 	
