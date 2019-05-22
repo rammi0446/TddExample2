973ff28239ae626cd439455a3b52c523594d5277
Navjot Gill <Navjot Gill@Raman>
2019-05-22 10:48:15 -0400

    R3-Red

--- test/SoftwareSaleTest.java
+++ test/SoftwareSaleTest.java
@@ -16,17 +16,26 @@ public class SoftwareSaleTest {
 
 	@Test
 	public void test() {
-//		SoftwareTest s = new SoftwareTest();
-//		double finalPrice = s.calculatePrice(2);
+//		SoftwareTest s1 = new SoftwareTest();
+//		double finalPrice = s1.calculatePrice(2);
 //		//when comapring decimal in juint you must add the third param to the assertEqual
 //		//for the third param use the minimum number like 0.000001 or 0
 //		assertEquals(198, finalPrice,0);
+		//
 		
-		SoftwareTest s = new SoftwareTest();
-		double finalPrice = s.calculatePrice(12);
+		//20% discount
+		SoftwareTest s2 = new SoftwareTest();
+		double finalPrice2 = s2.calculatePrice(12);
 		//when comapring decimal in juint you must add the third param to the assertEqual
 		//for the third param use the minimum number like 0.000001 or 0
-		assertEquals(198, finalPrice,0);
+		assertEquals(950.4, finalPrice2,0);
+		
+		//30% discount
+		SoftwareTest s3 = new SoftwareTest();
+		double finalPrice3 = s3.calculatePrice(30);
+		//when comapring decimal in juint you must add the third param to the assertEqual
+		//for the third param use the minimum number like 0.000001 or 0
+		assertEquals(950.4, finalPrice3,0);
 	}
 	
 	
