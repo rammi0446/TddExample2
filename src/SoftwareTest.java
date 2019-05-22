7d9b04ecb6bccf5fc80e6670134e77f7a2faafda
Navjot Gill <Navjot Gill@Raman>
2019-05-22 10:21:25 -0400

    R2-Green

--- src/SoftwareTest.java
+++ src/SoftwareTest.java
@@ -6,6 +6,13 @@ public class SoftwareTest {
 	//return the total price of the software package
 	public double calculatePrice(int quantity)
 	{
+		if(quantity >= 10 && quantity <= 19)
+		{
+			double subtotal = 99 * quantity;
+			double discount = subtotal * 0.20;
+			double finalTotal = subtotal - discount;
+			return finalTotal;
+		}
 		return ( 99.0 * quantity);
 	}
 	
